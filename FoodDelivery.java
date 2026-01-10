7109720b5615a7118de5f5c3d8f46d0cc4effc20
narmathaa07 <24004481@siswa.um.edu.my>
2026-01-10 17:52:46 +0800

    Add files via upload

---
+++ FoodDelivery.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fooddelivery;
+
+/**
+ *
+ * @author ASUS
+ */
+public class FoodDelivery {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
